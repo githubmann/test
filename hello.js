07108905ada36fcbd3ffc53ea3212da8fc5eb91c
githubmanm <1041763635@qq.com>
2016-01-31 22:08:31 +0800

    这是分支输出信息

--- hello.js
+++ hello.js
@@ -2,8 +2,9 @@
 * @Author: 波
 * @Date:   2016-01-31 21:24:39
 * @Last Modified by:   波
-* @Last Modified time: 2016-01-31 21:34:30
+* @Last Modified time: 2016-01-31 22:07:24
 */
 
 console.log('hello world');
 console.log('message');
+console.log('this is branch');
