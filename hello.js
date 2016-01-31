78a6b0c2ca83d72753265c2497033fd5b6c8306d
githubmanm <1041763635@qq.com>
2016-01-31 21:52:25 +0800

    增加了输出message信息

--- hello.js
+++ hello.js
@@ -2,7 +2,8 @@
 * @Author: 波
 * @Date:   2016-01-31 21:24:39
 * @Last Modified by:   波
-* @Last Modified time: 2016-01-31 21:24:51
+* @Last Modified time: 2016-01-31 21:34:30
 */
 
 console.log('hello world');
+console.log('message');
